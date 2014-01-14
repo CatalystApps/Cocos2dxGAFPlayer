b52fae26f2359b353031677d78cca288aca8ae96
Timur Losev <timur.losev@gmail.com>
2014-01-14 20:17:09 +0200

    - update an example. added content scale factor sample

--- Examples/Classes/AppDelegate.cpp
+++ Examples/Classes/AppDelegate.cpp
@@ -38,6 +38,33 @@ bool AppDelegate::applicationDidFinishLaunching()
 
     pDirector->setAnimationInterval(1.0 / kGlobalFPS);
 
+    CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
+
+    pEGLView->setDesignResolutionSize(2048, 1536, kResolutionNoBorder);
+
+    CCSize frameSize = pEGLView->getFrameSize();
+
+    // In this demo, we select resource according to the frame's height.
+    // If the resource size is different from design resolution size, you need to set contentScaleFactor.
+    // We use the ratio of resource's height to the height of design resolution,
+    // this can make sure that the resource's height could fit for the height of design resolution.
+
+    // if the frame's height is larger than the height of medium resource size, select large resource.
+    if (frameSize.height > 768)
+    {
+        pDirector->setContentScaleFactor(1536 / 768);
+    }
+    // if the frame's height is larger than the height of small resource size, select medium resource.
+    else if (frameSize.height > 320)
+    {
+        pDirector->setContentScaleFactor(768 / 320);
+    }
+    // if the frame's height is smaller than the height of medium resource size, select small resource.
+    else
+    {
+        pDirector->setContentScaleFactor(320 / 1536);
+    }
+
     //pDirector->setContentScaleFactor(2.f);
 
     // create a scene. it's an autorelease object
