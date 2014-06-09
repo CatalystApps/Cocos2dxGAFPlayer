0f207e89c3677e73cb05b60ade13d57f3029d4c0
Timur Losev <timur.losev@gmail.com>
2014-06-09 16:17:28 +0300

    - updated Android.mk

--- Android.mk
+++ Android.mk
@@ -35,6 +35,8 @@ Library/Sources/TagDefineAtlas.cpp \
 Library/Sources/TagDefineNamedParts.cpp \
 Library/Sources/TagDefineSequences.cpp \
 Library/Sources/TagDefineStage.cpp
+Library/Sources/TagDefineTimeline.cpp
+Library/Sources/TagDefineAnimationFrames2.cpp
 
 LOCAL_C_INCLUDES := \
 $(LOCAL_PATH)/../../cocos2dx/include \
