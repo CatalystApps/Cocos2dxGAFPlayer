ac74bd9bdfadd13978da69e3b4704fef926fa096
Timur Losev <timur.losev@gmail.com>
2014-06-17 12:13:07 +0300

    * fixed Android compilation

--- Android.mk
+++ Android.mk
@@ -34,8 +34,8 @@ Library/Sources/TagDefineAnimationObjects.cpp \
 Library/Sources/TagDefineAtlas.cpp \
 Library/Sources/TagDefineNamedParts.cpp \
 Library/Sources/TagDefineSequences.cpp \
-Library/Sources/TagDefineStage.cpp
-Library/Sources/TagDefineTimeline.cpp
+Library/Sources/TagDefineStage.cpp \
+Library/Sources/TagDefineTimeline.cpp \
 Library/Sources/TagDefineAnimationFrames2.cpp
 
 LOCAL_C_INCLUDES := \
