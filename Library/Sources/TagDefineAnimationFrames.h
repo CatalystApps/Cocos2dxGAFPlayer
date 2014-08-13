90ee57ba72819f65f11f023f564bc7a46b7b538e
Dmitry Bushtets <zsmartcat@gmail.com>
2014-08-13 18:30:00 +0300

    * fixed memory leak on scenes replace (http://gafmedia.com/forum/viewtopic.php?f=8&t=73)

--- Library/Sources/TagDefineAnimationFrames.h
+++ Library/Sources/TagDefineAnimationFrames.h
@@ -7,12 +7,17 @@ class GAFSubobjectState;
 class TagDefineAnimationFrames : public DefinitionTagBase
 {
 private:
+    typedef std::map<unsigned int, GAFSubobjectState*> States_t;
+    States_t m_currentStates;
+    
     GAFSubobjectState* extractState(GAFStream* in);
 
     void _translateColor(cocos2d::Color4F& out, const cocos2d::Color4B& in);
 
 public:
-
+    
+    ~TagDefineAnimationFrames();
+    
     virtual void read(GAFStream*, GAFAsset*);
 
 };
