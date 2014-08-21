05dc4530909614fde5d46adf7d550509f228f305
teivaz <teivaz@rambler.ru>
2014-08-21 17:32:40 +0300

    * Fixed GAFCachedTexture default constructor and copy operator

--- Library/Sources/GAFCachedTexture.cpp
+++ Library/Sources/GAFCachedTexture.cpp
@@ -19,6 +19,16 @@ cocos2d::Texture2D* GAFCachedTexture::operator *()
     return m_texture;
 }
 
+GAFCachedTexture::GAFCachedTexture()
+: m_texture(nullptr)
+, m_lastUsed(0)
+, m_created(0)
+, m_timesUsed(1)
+, m_memoryUsed(0)
+{
+
+}
+
 GAFCachedTexture::GAFCachedTexture(Texture2D* tex)
 : m_texture(tex)
 , m_lastUsed(0)
@@ -28,7 +38,7 @@ GAFCachedTexture::GAFCachedTexture(Texture2D* tex)
 {
     m_memoryUsed = tex->getBitsPerPixelForFormat() / 8 *
         static_cast<size_t>(tex->getContentSizeInPixels().width * tex->getContentSizeInPixels().height);
-    m_texture->retain();
+    CC_SAFE_RETAIN(m_texture);
     timeval now;
     gettimeofday(&now, NULL);
     m_created = now.tv_sec * 1000 + now.tv_usec;
@@ -37,12 +47,16 @@ GAFCachedTexture::GAFCachedTexture(Texture2D* tex)
 
 const GAFCachedTexture& GAFCachedTexture::operator =(const GAFCachedTexture& other)
 {
+    if (&other == this)
+        return *this;
+    if (m_texture == other.m_texture)
+        return *this;
     m_texture = other.m_texture;
     m_lastUsed = other.m_lastUsed;
     m_created = other.m_created;
     m_timesUsed = other.m_timesUsed;
     m_memoryUsed = other.m_memoryUsed;
-    m_texture->retain();
+    CC_SAFE_RETAIN(m_texture);
     return *this;
 }
 
@@ -53,10 +67,10 @@ GAFCachedTexture::GAFCachedTexture(const GAFCachedTexture& other)
 , m_timesUsed(other.m_timesUsed)
 , m_memoryUsed(other.m_memoryUsed)
 {
-    m_texture->retain();
+    CC_SAFE_RETAIN(m_texture);
 }
 
 GAFCachedTexture::~GAFCachedTexture()
 {
-    m_texture->release();
+    CC_SAFE_RELEASE(m_texture);
 }
