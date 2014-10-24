ab6331225f8c45ee6c01afdccf4d12ed87cf079b
teivaz <teivaz@rambler.ru>
2014-10-24 17:02:54 +0300

    * Fixed compatibility with cocos2d-x versions lower 3.3

--- Library/Sources/GAFSprite.cpp
+++ Library/Sources/GAFSprite.cpp
@@ -160,7 +160,11 @@ void GAFSprite::setAtlasScale(float scale)
 
 uint32_t GAFSprite::setUniforms()
 {
+#if COCOS2D_VERSION < 0x00030300
+    uint32_t materialID = QuadCommand::MATERIAL_ID_DO_NOT_BATCH;
+#else
     uint32_t materialID = Renderer::MATERIAL_ID_DO_NOT_BATCH;
+#endif
 
     if (_glProgramState->getUniformCount() == 0)
     {
