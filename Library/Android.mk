6e5b013954462327831ede82d6b187d5ff09d897
SmartCat <zsmartcat@gmail.com>
2014-12-01 18:09:10 +0200

    - updated Android.mk generation

--- Library/Android.mk
+++ Library/Android.mk
@@ -3,46 +3,47 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 CCX_ROOT := $(LOCAL_PATH)/../../..
+GAF_LIB_SOURCES := ./Sources
 
 LOCAL_MODULE := gafplayer_static
 
 LOCAL_MODULE_FILENAME := libgafplayer
 
-LOCAL_SRC_FILES := GAFPrecompiled.cpp \
-Sources/GAFObject.cpp \
-Sources/GAFMovieClip.cpp \
-Sources/GAFMask.cpp \
-Sources/GAFAssetTextureManager.cpp \
-Sources/GAFTimeline.cpp \
-Sources/GAFTimelineAction.cpp \
-Sources/GAFTextField.cpp \
-Sources/GAFTextData.cpp \
-Sources/GAFAnimationFrame.cpp \
-Sources/GAFAnimationSequence.cpp \
-Sources/GAFAsset.cpp \
-Sources/GAFFile.cpp \
-Sources/GAFFilterData.cpp \
-Sources/GAFLoader.cpp \
-Sources/GAFShaderManager.cpp \
-Sources/GAFQuadCommand.cpp \
-Sources/GAFSprite.cpp \
-Sources/GAFStream.cpp \
-Sources/GAFSubobjectState.cpp \
-Sources/GAFTextureAtlas.cpp \
-Sources/GAFTextureAtlasElement.cpp \
-Sources/GAFFilterManager.cpp \
-Sources/GAFCachedTexture.cpp \
-Sources/PrimitiveDeserializer.cpp \
-Sources/TagDefineAnimationFrames.cpp \
-Sources/TagDefineAnimationMasks.cpp \
-Sources/TagDefineAnimationObjects.cpp \
-Sources/TagDefineAtlas.cpp \
-Sources/TagDefineNamedParts.cpp \
-Sources/TagDefineSequences.cpp \
-Sources/TagDefineStage.cpp \
-Sources/TagDefineAnimationFrames2.cpp\
-Sources/TagDefineTimeline.cpp \
-Sources/TagDefineTextField.cpp 
+LOCAL_SRC_FILES := $(GAF_LIB_SOURCES)/GAFPrecompiled.cpp \
+$(GAF_LIB_SOURCES)/GAFObject.cpp \
+$(GAF_LIB_SOURCES)/GAFMovieClip.cpp \
+$(GAF_LIB_SOURCES)/GAFMask.cpp \
+$(GAF_LIB_SOURCES)/GAFAssetTextureManager.cpp \
+$(GAF_LIB_SOURCES)/GAFTimeline.cpp \
+$(GAF_LIB_SOURCES)/GAFTimelineAction.cpp \
+$(GAF_LIB_SOURCES)/GAFTextField.cpp \
+$(GAF_LIB_SOURCES)/GAFTextData.cpp \
+$(GAF_LIB_SOURCES)/GAFAnimationFrame.cpp \
+$(GAF_LIB_SOURCES)/GAFAnimationSequence.cpp \
+$(GAF_LIB_SOURCES)/GAFAsset.cpp \
+$(GAF_LIB_SOURCES)/GAFFile.cpp \
+$(GAF_LIB_SOURCES)/GAFFilterData.cpp \
+$(GAF_LIB_SOURCES)/GAFLoader.cpp \
+$(GAF_LIB_SOURCES)/GAFShaderManager.cpp \
+$(GAF_LIB_SOURCES)/GAFQuadCommand.cpp \
+$(GAF_LIB_SOURCES)/GAFSprite.cpp \
+$(GAF_LIB_SOURCES)/GAFStream.cpp \
+$(GAF_LIB_SOURCES)/GAFSubobjectState.cpp \
+$(GAF_LIB_SOURCES)/GAFTextureAtlas.cpp \
+$(GAF_LIB_SOURCES)/GAFTextureAtlasElement.cpp \
+$(GAF_LIB_SOURCES)/GAFFilterManager.cpp \
+$(GAF_LIB_SOURCES)/GAFCachedTexture.cpp \
+$(GAF_LIB_SOURCES)/PrimitiveDeserializer.cpp \
+$(GAF_LIB_SOURCES)/TagDefineAnimationFrames.cpp \
+$(GAF_LIB_SOURCES)/TagDefineAnimationMasks.cpp \
+$(GAF_LIB_SOURCES)/TagDefineAnimationObjects.cpp \
+$(GAF_LIB_SOURCES)/TagDefineAtlas.cpp \
+$(GAF_LIB_SOURCES)/TagDefineNamedParts.cpp \
+$(GAF_LIB_SOURCES)/TagDefineSequences.cpp \
+$(GAF_LIB_SOURCES)/TagDefineStage.cpp \
+$(GAF_LIB_SOURCES)/TagDefineAnimationFrames2.cpp\
+$(GAF_LIB_SOURCES)/TagDefineTimeline.cpp \
+$(GAF_LIB_SOURCES)/TagDefineTextField.cpp 
 
 LOCAL_C_INCLUDES := \
 $(CCX_ROOT)/cocos \
@@ -50,6 +51,6 @@ $(CCX_ROOT)/cocos/2d \
 $(CCX_ROOT)/cocos/platform/android \
 $(CCX_ROOT)/cocos/math \
 $(CCX_ROOT)/extensions \
-$(LOCAL_PATH)/Sources
+$(GAF_LIB_SOURCES)
 
 include $(BUILD_STATIC_LIBRARY)
