34cbe110ac62b0dccd147629dac6eba10e006a8a
teivaz <teivaz@rambler.ru>
2015-01-16 18:18:36 +0200

    - [JSB] Removed some functions from bindings - [JSB] Updated bindings - [JSB] Implemented conversion GAFHeader to JS object

--- js_bindings/bindings/js_gaf_manual_conversions.cpp
+++ js_bindings/bindings/js_gaf_manual_conversions.cpp
@@ -3,54 +3,61 @@
 
 USING_NS_GAF;
 
-jsval AnimationMasks_to_jsval(JSContext *cx, const AnimationMasks_t& v)
-{/*
- JSObject *tmp = JS_NewObject(cx, NULL, NULL, NULL);
- if (!tmp) return JSVAL_NULL;
- bool ok = JS_DefineProperty(cx, tmp, "x", DOUBLE_TO_JSVAL(v.x), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
- JS_DefineProperty(cx, tmp, "y", DOUBLE_TO_JSVAL(v.y), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT);
- if (ok) {
- return OBJECT_TO_JSVAL(tmp);
- }*/
-    return JSVAL_NULL;
+jsval gaf_std_vector_float_to_jsval(JSContext *cx, const std::vector<float>& v)
+{
+    JSObject *jsretArr = JS_NewArrayObject(cx, 0, NULL);
+
+    int i = 0;
+    for (const float obj : v)
+    {
+        JS::RootedValue arrElement(cx);
+        arrElement = DOUBLE_TO_JSVAL(obj);
+
+        if (!JS_SetElement(cx, jsretArr, i, &arrElement)) {
+            break;
+        }
+        ++i;
+    }
+    return OBJECT_TO_JSVAL(jsretArr);
 }
 
-jsval AnimationObjects_to_jsval(JSContext *cx, const AnimationObjects_t& v)
+jsval GAFHeader_to_jsval(JSContext *cx, const GAFHeader& header)
 {
-    return JSVAL_NULL;
-}
+	JSObject *tmp = JS_NewObject(cx, NULL, NULL, NULL);
+	if (!tmp){
+		return JSVAL_NULL;
+	}
+    uint16_t majorVersion = header.getMajorVersion();
+    bool ok = JS_DefineProperty(cx, tmp, "compression", UINT_TO_JSVAL(header.compression), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+        JS_DefineProperty(cx, tmp, "versionMajor", INT_TO_JSVAL(majorVersion), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+        JS_DefineProperty(cx, tmp, "versionMinor", INT_TO_JSVAL(header.getMinorVersion()), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+        JS_DefineProperty(cx, tmp, "fileLength", UINT_TO_JSVAL(header.fileLenght), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT);
 
-jsval AnimationFrames_to_jsval(JSContext *cx, const AnimationFrames_t& v)
-{
-    return JSVAL_NULL;
-}
+	if (!ok)
+    {
+        return JSVAL_NULL;
+    }
 
-jsval NamedParts_to_jsval(JSContext *cx, const NamedParts_t& v)
-{
-    return JSVAL_NULL;
-}
+    if (majorVersion < 4)
+    {
+        jsval frameSize = ccrect_to_jsval(cx, header.frameSize);
+        jsval pivot = ccpoint_to_jsval(cx, header.pivot);
+        ok = JS_DefineProperty(cx, tmp, "framesCount", INT_TO_JSVAL(header.compression), NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+            JS_DefineProperty(cx, tmp, "frameSize", frameSize, NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+            JS_DefineProperty(cx, tmp, "pivot", pivot, NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT);
+    }
+    else
+    {
+        jsval scaleValues = gaf_std_vector_float_to_jsval(cx, header.scaleValues);
+        jsval csfValues = gaf_std_vector_float_to_jsval(cx, header.csfValues);
+        ok = JS_DefineProperty(cx, tmp, "scaleValues", scaleValues, NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT) &&
+            JS_DefineProperty(cx, tmp, "csfValues", csfValues, NULL, NULL, JSPROP_ENUMERATE | JSPROP_PERMANENT);
+    }
 
-jsval AnimationSequences_to_jsval(JSContext *cx, const AnimationSequences_t& v)
-{
-    return JSVAL_NULL;
-}
+    if (!ok)
+    {
+        return JSVAL_NULL;
+    }
 
-jsval TextsData_to_jsval(JSContext *cx, const TextsData_t& v)
-{
-    return JSVAL_NULL;
-}
-
-jsval GAFTextureAtlas_to_jsval(JSContext *cx, const GAFTextureAtlas& v)
-{
-    return JSVAL_NULL;
-}
-
-jsval Timelines_to_jsval(JSContext *cx, const Timelines_t& v)
-{
-    return JSVAL_NULL;
-}
-
-jsval GAFHeader_to_jsval(JSContext *cx, const GAFHeader& v)
-{
-    return JSVAL_NULL;
+    return OBJECT_TO_JSVAL(tmp);
 }
