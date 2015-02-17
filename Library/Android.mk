95d7c05b894059521f414dbe305e4ff820f32f78
SmartCat <zsmartcat@gmail.com>
2015-02-17 15:06:15 +0200

    * fixed Android.mk

--- Library/Android.mk
+++ Library/Android.mk
@@ -15,7 +15,9 @@ LOCAL_SRC_FILES := $(CLASSES_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := \
 $(CCX_ROOT)/cocos \
+$(CCX_ROOT)/cocos/platform/android \
 $(CCX_ROOT_2)/cocos \
+$(CCX_ROOT_2)/cocos/platform/android \
 $(GAF_LIB_SOURCES)
 
 include $(BUILD_STATIC_LIBRARY)
