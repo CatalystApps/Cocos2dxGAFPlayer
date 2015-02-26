2cb3e49e98bcdee23ba6af296cd584b24d9f399e
SmartCat <zsmartcat@gmail.com>
2015-02-26 17:18:12 +0200

    - [TextField] changed color setter

--- Library/Sources/GAFTextField.cpp
+++ Library/Sources/GAFTextField.cpp
@@ -19,8 +19,7 @@ void GAFTextField::initWithTextData(GAFTextData const* data)
     m_label = cocos2d::Label::create(data->m_text, data->m_textFormat.m_font, data->m_textFormat.m_size);
     
     m_label->setDimensions(data->m_width, data->m_height);
-    //m_label->setTextColor(data->m_textFormat.m_color); // NOT WORKING
-    m_label->setColor(cocos2d::Color3B(data->m_textFormat.m_color));
+    m_label->setTextColor(cocos2d::Color4B(data->m_textFormat.m_color));
     m_label->setHorizontalAlignment(data->m_textFormat.getTextAlignForCocos());
 
     cocos2d::Point anchor = cocos2d::Point(data->m_pivot.x / data->m_width, 1 - data->m_pivot.y / data->m_height);
