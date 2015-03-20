2175e6f119480b906a8575ec0fae5b717954476c
SmartCat <zsmartcat@gmail.com>
2015-03-20 16:07:07 +0200

    - [jsb] modified Android.mk for bindings

--- js_bindings/Android.mk
+++ js_bindings/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 CCX_ROOT := $(LOCAL_PATH)/../../..
 CCX_ROOT_2 := $(LOCAL_PATH)/../../cocos2d-x
-GAF_LIB_SOURCES := $(LOCAL_PATH)/Library/Sources
+GAF_LIB_SOURCES := $(LOCAL_PATH)/../Library/Sources
 GAF_BINDINGS_SOURCES := $(LOCAL_PATH)/bindings
 
 LOCAL_MODULE := gafjsbindings_static
@@ -17,8 +17,16 @@ LOCAL_SRC_FILES := $(CLASSES_FILES:$(LOCAL_PATH)/%=%)
 LOCAL_C_INCLUDES := \
 $(CCX_ROOT)/cocos \
 $(CCX_ROOT)/cocos/platform/android \
+$(CCX_ROOT)/plugin/jsbindings/manual \
+$(CCX_ROOT)/../bindings/manual \
 $(CCX_ROOT_2)/cocos \
 $(CCX_ROOT_2)/cocos/platform/android \
-$(GAF_LIB_SOURCES)
+$(CCX_ROOT_2)/plugin/jsbindings/manual \
+$(CCX_ROOT_2)/../bindings/manual \
+$(GAF_LIB_SOURCES) \
+
+LOCAL_STATIC_LIBRARIES := spidermonkey_static
 
 include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,external/spidermonkey/prebuilt/android)
